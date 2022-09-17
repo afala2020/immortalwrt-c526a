3fea2d112fab4dbc6042caf9223c0c3bd94c6c84
afala2020 <71696868+afala2020@users.noreply.github.com>
2022-09-17 20:07:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,19 +18,5 @@
 #sed -i '2d' feeds.conf.default
 #sed -i '$d' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git custom https://github.com/kiddin9/openwrt-packages' feeds.conf.default
-sed -i '$a src-git bypass https://github.com/kiddin9/openwrt-bypass' feeds.conf.default
-#sed -i '$a src-git smartdns-le https://github.com/garypang13/smartdns-le' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-#sed -i '$a src-git litte https://github.com/kenzok8/litte' feeds.conf.default
-#sed -i '$a src-git maxminddb https://github.com/jerrykuku/lua-maxminddb.git' feeds.conf.default
-#sed -i '$a src-git vssr https://github.com/jerrykuku/luci-app-vssr.git' feeds.conf.default
-# Add Theme
-#sed -i '$a src-git infi https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' feeds.conf.default
-#sed -i '$a src-git infi https://github.com/kenzok8/luci-theme-ifit' feeds.conf.default
-git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
